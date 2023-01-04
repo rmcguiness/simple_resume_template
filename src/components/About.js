a284aa71a0a363808ae16a34d9ad3c53bcd952b6
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-01-04 17:38:54 -0500

    added content to basic template

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import '../styles/About.css';
+import profilePic from '../images/img_profile.jpeg'
 
 
 const About = () => {
   return (
     <div className='about-container'>
-        <div className='about-content'>About</div>
+        <img src={profilePic} className='profile-img' alt='profile' />
+        <h1>Ryan McGuiness</h1>
+        <div className='description'>
+            <h5>A hardworking and intuitive problem solver that desires to be better each day.</h5>
+        </div> 
     </div>
   )
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,40 @@ import '../styles/Resume.css';
 const Resume = () => {
   return (
     <div className="resume-container">
-        <div className='resume-content'>Resume</div>
+        <div className='side-bar'>
+            <div className='education'>
+                <h2>Education</h2>
+                <div className='school'>
+                    <h4>Lehigh University</h4>
+                    <p className='school-date'>Jan 2018 - May 2021</p>
+                </div>
+                <p className='education-desc'>B.A in Computer Science</p>
+            </div>
+            <div className='skills'>
+                <h2>Skills</h2>
+                <p>Python • PHP • Java • C/C++ • JavaScript • TypeScript • React • React-Native • Git</p>
+            </div>
+        </div>
+        <div className='experience'>
+            <h2>Work Experience</h2>
+            <div className='job'>
+                <h4>Perficient</h4>
+                <p className='job-title'>Software Engineer</p>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </li>
+                    <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+                </ul>
+            </div>
+            <div className='job'>
+                <h4>Hello World</h4>
+                <p className='job-title'>Software Developer</p>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </li>
+                    <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+                </ul>
+            </div>
+        </div>
+        
     </div>
     
   )
